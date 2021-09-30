c4cde2fd6a6c79c21f9312c4056617a8903bce7c
CHINGHSIEN <86924031+Zora0725@users.noreply.github.com>
2021-09-30 19:46:54 +0800

    Add files via upload
    homework

--- test5.java
+++ test5.java
@@ -1,21 +1,37 @@
-
+import java.util.Scanner;
 public class test5 {
 	
 	public static void main(String[] args) {
+
+	// 隨機亂數假設(0,99)之間的亂數相加
+	int x = (int) (Math.random() *100);
+	
+	//OUTPUT隨機亂數相加請USER輸入答案
+	System.out.println("Guess number game (0-99)");
+	Scanner input = new Scanner(System.in);
+	int answer = input.nextInt();
+	
+	if (answer == x) {
+		System.out.println("Correct.");
 		
-		int x = 1;
-		System.out.println(x); //OUTPUT1
+	} else {
+		System.out.println("Wrong.");
 		
-		x = x + 1;
-		System.out.println(x); //Output 2
-		
-		x += 2;
-		System.out.println(x); //OutPut4
-		
-		x++; 
-		System.out.println(x);
-		
+	// 答錯時
+	while (answer != x) {
+		System.out.println("Wrong, Try again?");
+		answer = input.nextInt();
+	}
+	
+	
+	System.out.println("The answer is " + x + ".");
+	System.out.println("Correct.");
+	
+	
+		input.close();
 		
 	}
-
+	
+	 }
+	 
 }
